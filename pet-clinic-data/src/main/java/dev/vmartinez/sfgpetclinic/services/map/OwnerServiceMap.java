13615b802b01df691108a96efb3926a53736438e
Victor Martinez <victor.eod2@gmail.com>
2021-06-02 02:44:05 -0400

    Added Spring config for service maps. Closes #21.

--- pet-clinic-data/src/main/java/dev/vmartinez/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/dev/vmartinez/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package dev.vmartinez.sfgpetclinic.services.map;
 
 import dev.vmartinez.sfgpetclinic.model.Owner;
 import dev.vmartinez.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
     @Override
     public Set<Owner> findAll() {
--- pet-clinic-data/src/main/java/dev/vmartinez/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/dev/vmartinez/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package dev.vmartinez.sfgpetclinic.services.map;
 
 import dev.vmartinez.sfgpetclinic.model.Pet;
 import dev.vmartinez.sfgpetclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
